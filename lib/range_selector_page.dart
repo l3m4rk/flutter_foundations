bde4e24e8c21b10d2c82a97255858bd020d5ca62
Dmitry <b4r4n0v@gmail.com>
2022-11-12 21:20:36 +0200

    first commit

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foundations/range_selector_form.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  const RangeSelectorPage({super.key});
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  final formKey = GlobalKey<FormState>();
+  int _min = 0;
+  int _max = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Range'),
+      ),
+      body: RangeSelectorForm(
+        formKey: formKey,
+        minValueSetter: (value) => _min = value,
+        maxValueSetter: (value) => _max = value,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_forward),
+        onPressed: () {
+          if (formKey.currentState?.validate() == true) {
+            formKey.currentState?.save();
+            // TODO: navigate to the generator page
+          }
+        },
+      ),
+    );
+  }
+}
